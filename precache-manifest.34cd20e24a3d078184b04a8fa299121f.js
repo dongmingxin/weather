71eb75e69b4377a20c179f954db901337d35feed
Mingxin Dong <836514519dmx@gmail.com>
2020-05-04 01:21:23 +1000

    Updates

---
+++ precache-manifest.34cd20e24a3d078184b04a8fa299121f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a35fa0c9149c54b25348ba8e1a3807fb",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "dd0500f52dffddfcd278",
+    "url": "/weather/static/css/main.f7a17f83.chunk.css"
+  },
+  {
+    "revision": "64b3d544448e7431d70b",
+    "url": "/weather/static/js/2.ca59b294.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather/static/js/2.ca59b294.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd0500f52dffddfcd278",
+    "url": "/weather/static/js/main.7449ce0f.chunk.js"
+  },
+  {
+    "revision": "93f41cbaddd6189c9424",
+    "url": "/weather/static/js/runtime-main.b405a412.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/weather/static/media/logo.5d5d9eef.svg"
+  }
+]);
