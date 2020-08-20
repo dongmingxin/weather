2922c69680a99ab943e8ac07579dcac12d54ef33
Mingxin Dong <836514519dmx@gmail.com>
2020-08-20 22:32:14 +1000

    add conflict

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,9 +3,10 @@ class Footer extends Component {
     state = {  }
     render() { 
         return ( 
-            <React.Fragment>
-                <footer class="weather-channel__footer"><p>Powered by React</p></footer>
-            </React.Fragment>
+            // <React.Fragment>
+            //     <footer class="weather-channel__footer"><p>Powered by React</p></footer>
+            // </React.Fragment>
+            <div>aa</div>
          );
     }
 }
